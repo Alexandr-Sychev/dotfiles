b94d3e41bf5f1eba0d1d5bc0905b3976feca100e
Alexandr Sychev <alx.sychevvv@gmail.com>
2022-12-19 06:06:19 +0300

    remove echo

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-echo  $DOTFILES
 ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
-
-echo $DOTFILES/tmux/tmux.conf
 
 git clone https://github.com/tmux-plugins/tpm.git $HOME/.tmux/plugins/tpm
