da9cd9a845030547a2bb8d0750a8f3c8a5e9d2f6
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-15 14:07:04 +0300

    .

--- roles/awesomewm/files/modules/tags.lua
+++ roles/awesomewm/files/modules/tags.lua
@@ -1,21 +1,31 @@
 local awful = require("awful")
 
--- local screen_number = 2
+local function empty(s)
+    return s == nil or s == ''
+end
 
-function add_tags(tags)
-    local screen_number
+local function choose_screen_number(isSecondary)
+    if isSecondary then
+        return 1
+    else
+        return 2
+    end
+end
 
+local function add_tags(tags)
     for i, tag in ipairs(tags) do
-        if tag.secondary then
-            screen_number = 1
-        else
-            screen_number = 2
-        end
+        local screen_number = choose_screen_number(tag.secondary)
 
         awful.tag.add(tag.name, {
             screen = screen_number,
             layout = awful.layout.layouts[1],
         })
+
+        if not empty(tag.command) then
+            awful.spawn(tag.command, {
+                tag = awful.tag.find_by_name(awful.screen.focused(), tag.name)
+            })
+        end
     end
 end
 
