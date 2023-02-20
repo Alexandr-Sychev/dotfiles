ebdf3aac18754b0c328811f2cd51b7c8ccb9d72f
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-20 21:53:38 +0300

    .

--- roles/awesomewm/files/modules/key_bindings.lua
+++ roles/awesomewm/files/modules/key_bindings.lua
@@ -96,25 +96,26 @@ globalkeys = gears.table.join(
                 )
             end
         end,
-        { description = "restore minimized", group = "client" }),
+        { description = "restore minimized", group = "client" })
 
-    -- Prompt
-    awful.key({ modkey }, "r", function() awful.screen.focused().mypromptbox:run() end,
-        { description = "run prompt", group = "launcher" }),
+-- Prompt
+-- awful.key({ modkey }, "r", function() awful.screen.focused().mypromptbox:run() end,
+--     { description = "run prompt", group = "launcher" }),
+--
+-- awful.key({ modkey }, "x",
+--     function()
+--         awful.prompt.run {
+--             prompt       = "Run Lua code: ",
+--             textbox      = awful.screen.focused().mypromptbox.widget,
+--             exe_callback = awful.util.eval,
+--             history_path = awful.util.get_cache_dir() .. "/history_eval"
+--         }
+--     end,
+--     { description = "lua execute prompt", group = "awesome" }),
 
-    awful.key({ modkey }, "x",
-        function()
-            awful.prompt.run {
-                prompt       = "Run Lua code: ",
-                textbox      = awful.screen.focused().mypromptbox.widget,
-                exe_callback = awful.util.eval,
-                history_path = awful.util.get_cache_dir() .. "/history_eval"
-            }
-        end,
-        { description = "lua execute prompt", group = "awesome" }),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-        { description = "show the menubar", group = "launcher" })
+-- Menubar
+-- awful.key({ modkey }, "p", function() menubar.show() end,
+--     { description = "show the menubar", group = "launcher" })
 )
 
 -- Bind all key numbers to tags.
