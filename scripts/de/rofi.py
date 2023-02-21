363450ad6d1a2ff977b7e2dca70300f745492ba5
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-22 00:57:33 +0300

    .

--- scripts/de/rofi.py
+++ scripts/de/rofi.py
@@ -5,7 +5,7 @@ import os
 
 def choose_with_rofi(prompt, variants):
     variants_string = '\n'.join(variants)
-    command = f'printf "{variants_string}" | rofi -dmenu -p "{prompt}"'
+    command = f'printf "{variants_string}" | rofi -dmenu -i -p "{prompt}"'
     stream = os.popen(command)
     return stream.read()[:-1]
 
