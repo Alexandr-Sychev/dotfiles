957af1f0bd7aea5b6df4c5721520f534ebd007aa
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-23 02:48:14 +0300

    .

--- roles/nvim/files/snippets/php.lua
+++ roles/nvim/files/snippets/php.lua
@@ -14,14 +14,30 @@ local rep = require("luasnip.extras").rep
 local snippets, autosnippets = {}, {}
 
 local group = vim.api.nvim_create_augroup("Lua Snippets", { clear = true })
-local file_pattern = "*.lua"
+local file_pattern = "*.php"
+
 
 -- Start
 
+-- WE NEED
+--
+-- class - cls <name>
+-- method - fn <name>
+--
+-- anonymous function - fn
+-- arrow function - afn
+-- inline arrow function - ifn
+--
+-- if else
+-- while
+-- do while
+-- for
+-- foreach - fre <array_name>
+--
+-- getters - get <...fields>
+-- setters - set <...fields>
+
 -- End
-
-
-
 
 
 return snippets, autosnippets
