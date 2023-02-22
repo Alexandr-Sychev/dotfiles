2c9a50aad6b360ca4140202281c257818608e629
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-22 19:01:06 +0300

    .

--- roles/nvim/files/lua/alx/remap.lua
+++ roles/nvim/files/lua/alx/remap.lua
@@ -1,8 +1,15 @@
 vim.g.mapleader = " "
 
+
 local telescopeBuiltin = require("telescope.builtin");
 
--- search and navigation
+
+-- packer
+vim.keymap.set("n", "<leader>ps", ':PackerStatus\n')
+vim.keymap.set("n", "<leader>pS", ':PackerSync\n')
+
+
+-- files navigation and search
 -- all not ignored files
 vim.keymap.set("n", "<leader>ff", telescopeBuiltin.find_files, {})
 -- recent files
@@ -14,41 +21,52 @@ vim.keymap.set("n", "<leader>gw", telescopeBuiltin.grep_string, {})
 -- open last closed file
 vim.keymap.set("n", "<leader><leader>", ':e#\n')
 
--- rename
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
--- comment
-vim.keymap.set("n", "<leader>/", ':CommentToggle\n')
-vim.keymap.set("v", "<leader>/", ':CommentToggle\n')
 
+-- files tree
+-- toggle files tree
+vim.keymap.set("n", "<leader>e", ':NvimTreeToggle\n')
+-- show current file in files tree
+vim.keymap.set("n", "<leader>E", ':NvimTreeFindFile\n')
+
+
+-- splits
 -- splits navigation
 vim.keymap.set("n", "<C-h>", '<C-w>h')
 vim.keymap.set("n", "<C-j>", '<C-w>j')
 vim.keymap.set("n", "<C-k>", '<C-w>k')
 vim.keymap.set("n", "<C-l>", '<C-w>l')
-
 -- splits resizing
 vim.keymap.set("n", "<C-A-l>", '<C-w>10>')
 vim.keymap.set("n", "<C-A-h>", '<C-w>10<')
 vim.keymap.set("n", "<C-A-j>", '<C-w>5+')
 vim.keymap.set("n", "<C-A-k>", '<C-w>5-')
 
--- toggle files tree
-vim.keymap.set("n", "<leader>e", ':NvimTreeToggle\n')
--- show current file in files tree
-vim.keymap.set("n", "<leader>E", ':NvimTreeFindFile\n')
 
--- packer
-vim.keymap.set("n", "<leader>ps", ':PackerStatus\n')
-vim.keymap.set("n", "<leader>pS", ':PackerSync\n')
+-- buffer navigaion
+-- stay on center during navigation
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+
+
+-- buffer actions
+-- rename
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+-- comment
+vim.keymap.set("n", "<leader>/", ':CommentToggle\n')
+vim.keymap.set("v", "<leader>/", ':CommentToggle\n')
+
 
 -- code
 -- show signature
 vim.keymap.set("n", "<leader>s", vim.lsp.buf.signature_help, bufopts)
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, bufopts)
 
+
 -- floaterm
 -- open terminal
-vim.keymap.set("n", "<leader>t", ':FloatermNew --width=0.9 --height=0.95 --title=terminal<CR>')
+vim.keymap.set("n", "<leader>t", ':FloatermNew --width=0.4 --wintype=vsplit --title=terminal zsh <CR>')
 -- open lazygit
 vim.keymap.set("n", "<leader>G", ':FloatermNew --width=0.9 --height=0.95 --title=lazygit lazygit <CR>')
 -- open lazydocker
