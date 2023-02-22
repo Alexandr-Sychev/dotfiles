9152d1b9d71e52d645f68bd3c3f4953225039915
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-22 04:09:26 +0300

    .

--- scripts/de/open-power-management-menu.py
+++ scripts/de/open-power-management-menu.py
@@ -5,10 +5,10 @@ import rofi
 prompt = 'Power management'
 
 variants = {
-    'Lock': '~/.dotfiles/scripts/screen-locker/lockscreen',
-    'Sleep': 'systemctl suspend',
-    'Power off': 'poweroff',
-    'Restart': 'reboot',
+    '1 Lock': '~/.dotfiles/scripts/screen-locker/lockscreen',
+    '2 Sleep': 'systemctl suspend',
+    '3 Restart': 'reboot',
+    '4 Power off': 'poweroff',
 }
 
 rofi.choose_with_rofi_and_execute_command(prompt, variants)
