5881c419de879043902cd21981f18982cdf24a98
Alexandr Sychev <alx.sychevvv@gmail.com>
2023-02-23 04:37:14 +0300

    .

--- roles/nvim/files/after/plugin/telescope.lua
+++ roles/nvim/files/after/plugin/telescope.lua
@@ -1,4 +1,14 @@
 require('telescope').setup {
+    defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+            },
+        },
+        sorting_strategy = "ascending",
+    },
+
     pickers = {
         find_files = {
             no_ignore = true,
