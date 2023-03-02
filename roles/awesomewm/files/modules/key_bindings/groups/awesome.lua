3d78b089f73c0234635fe59755df8fa27a0214e2
Alexandr Sychev <alx.sychevvv@gmail>
2023-03-03 01:16:40 +0300

    .

--- roles/awesomewm/files/modules/key_bindings/groups/awesome.lua
+++ roles/awesomewm/files/modules/key_bindings/groups/awesome.lua
@@ -9,6 +9,15 @@ local lock_screen = '~/.dotfiles/scripts/screen-locker/lockscreen'
 local power_manager = '~/.dotfiles/scripts/de/open-power-management-menu.py'
 local bluetooth_manager = '/bin/rofi-bluetooth/rofi-bluetooth'
 
+local hide_wibox = function()
+    for s in screen do
+        s.mywibox.visible = not s.mywibox.visible
+        if s.mybottomwibox then
+            s.mybottomwibox.visible = not s.mybottomwibox.visible
+        end
+    end
+end
+
 return {
     k('m', 'l', 'lockscreen', ss(lock_screen)),
     k('m', 'p', 'open power manager', ss(power_manager)),
@@ -16,4 +25,5 @@ return {
     k('ms', 'r', 'reload awesome', awesome.restart),
     k('m', 'w', 'show main menu', function() mymainmenu:show() end),
     k('ms', 'h', 'show help', hotkeys_popup.show_help),
+    k('m', 'v', 'hide wibox', hide_wibox),
 }
--- roles/awesomewm/files/modules/key_bindings/groups/screen.lua
+++ roles/awesomewm/files/modules/key_bindings/groups/screen.lua
@@ -1,16 +1,8 @@
-local awful = require("awful")
+local awful = require('awful')
+local utils = require('modules.key_bindings.utils')
+
+local k = utils.create_keybinding_builder('screen')
 
 return {
-
-
-    awful.key({ modkey, }, "o", function() awful.screen.focus_relative(1) end,
-        { description = "focus the next screen", group = "screen" }),
-    awful.key({ modkey, }, "u",
-        function() awful.screen.focus(1) end,
-        { description = "open secondary screen", group = "screen" }),
-
-    awful.key({ modkey, }, "i",
-        function() awful.screen.focus(2) end,
-        { description = "open main screen", group = "screen" }),
-
+    k('m', 'o', 'focus the next screen', function() awful.screen.focus_relative(1) end),
 }
