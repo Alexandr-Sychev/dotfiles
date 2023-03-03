09e3a46c1e3a20cc223c7ea5eb311b225556c173
Alexandr Sychev <alx.sychevvv@gmail>
2023-03-03 08:37:26 +0300

    .

--- roles/zsh/files/config/index.zsh
+++ roles/zsh/files/config/index.zsh
@@ -20,8 +20,6 @@ bindkey -v
 export FZF_PREVIEW_ADVANCED=true
 export FZF_PREVIEW_WINDOW='right:65%:nohidden'
 
-export EDITOR="nvim"
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
