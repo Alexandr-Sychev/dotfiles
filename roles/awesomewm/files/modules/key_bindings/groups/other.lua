102cf7c082c59ffddf10c287a2b2775af85dadb8
Alexandr Sychev <alx.sychevvv@gmail>
2023-03-05 18:03:01 +0300

    .

--- roles/awesomewm/files/modules/key_bindings/groups/other.lua
+++ roles/awesomewm/files/modules/key_bindings/groups/other.lua
@@ -4,7 +4,7 @@ local utils = require('modules.key_bindings.utils')
 local k = utils.create_keybinding_builder('other')
 local ss = utils.spawn_with_shell
 
-watch_later_cmd = '/bin/bash -c /home/alx/.dotfiles/scripts/watch-later'
+watch_later_cmd = '/bin/bash -c /home/alx/.dotfiles/scripts/add-to-watch-later'
 
 return {
     k('m', 'b', 'add to watch later list', ss(watch_later_cmd))
