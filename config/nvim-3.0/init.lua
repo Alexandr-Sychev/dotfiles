07d69206cf6b4f144aead547c9f8b4cce595ff1b
Alexandr Sychev <alx.sychevvv@gmail>
2024-02-04 16:45:06 +0300

    .

--- config/nvim-3.0/init.lua
+++ config/nvim-3.0/init.lua
@@ -19,29 +19,42 @@ require("packer").startup(function(use)
         config = function() return require("tmux").setup() end
     })
     use "ellisonleao/gruvbox.nvim"
+    use "terrortylor/nvim-comment"
 end)
+
+-- utils
+function kn(...) vim.keymap.set("n", unpack({...})) end
+function kv(...) vim.keymap.set("v", unpack({...})) end
+function knv(...)
+    kn(unpack({...}))
+    kv(unpack({...}))
+end
+function t(command) return "<cmd>Telescope " .. command .. " <CR>" end
+
+-- open vim config
+kn("<leader>C", ":e ~/.config/nvim/init.lua<cr>")
 
 -- buffer navigaion
 -- stay on center during navigation
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "G", "Gzz")
+kn("<C-d>", "<C-d>zz")
+kn("<C-u>", "<C-u>zz")
+kn("n", "nzz")
+kn("N", "Nzz")
+kn("G", "Gzz")
 
 -- splits
 -- create
-vim.keymap.set("n", "<leader>v", ':vsp <CR>', { silent = true })
+kn("<leader>v", ':vsp <CR>', { silent = true })
 -- navigate
-vim.keymap.set("n", "<C-h>", '<C-w>h')
-vim.keymap.set("n", "<C-j>", '<C-w>j')
-vim.keymap.set("n", "<C-k>", '<C-w>k')
-vim.keymap.set("n", "<C-l>", '<C-w>l')
+kn("<C-h>", '<C-w>h')
+kn("<C-j>", '<C-w>j')
+kn("<C-k>", '<C-w>k')
+kn("<C-l>", '<C-w>l')
 -- resize
-vim.keymap.set("n", "<C-A-l>", '<C-w>10>')
-vim.keymap.set("n", "<C-A-h>", '<C-w>10<')
-vim.keymap.set("n", "<C-A-j>", '<C-w>5+')
-vim.keymap.set("n", "<C-A-k>", '<C-w>5-')
+kn("<C-A-l>", '<C-w>10>')
+kn("<C-A-h>", '<C-w>10<')
+kn("<C-A-j>", '<C-w>5+')
+kn("<C-A-k>", '<C-w>5-')
 
 -- buffer search
 vim.keymap.set('n', '<leader>s', function()
@@ -55,16 +68,17 @@ end)
 
 -- files navigation and search
 -- directory view
--- vim.keymap.set("n", "<leader>fl", ':Explore\n', { silent = true })
-vim.keymap.set("n", "<leader>fl", telescopeBuiltin.find_files, {})
+kn("<leader>fl", telescopeBuiltin.find_files, {})
 -- all not ignored files
-vim.keymap.set("n", "<leader>ff", telescopeBuiltin.git_files, {})
+kn("<leader>ff", telescopeBuiltin.git_files, {})
 -- recent files
-vim.keymap.set("n", "<leader>fr", telescopeBuiltin.oldfiles, {})
+kn("<leader>fr", telescopeBuiltin.oldfiles, {})
 -- grep
-vim.keymap.set("n", "<leader>fg", telescopeBuiltin.live_grep, {})
+kn("<leader>fg", telescopeBuiltin.live_grep, {})
+-- edited files
+kn("<leader>fe", t("git_status"))
 -- open last closed file
-vim.keymap.set("n", "<leader><leader>", ':e#\n', { silent = true })
+kn("<leader><leader>", ':e#\n', { silent = true })
 
 -- autocomplete
 local cmp = require "cmp"
@@ -91,27 +105,12 @@ cmp.setup {
     },
 }
 
+-- comment
+require('nvim_comment').setup()
+knv("<leader>/", ":CommentToggle<CR>", { silent = true })
+
 -- lsp
-lsp_on_attach = function(client)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-
-    vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-    vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-    vim.keymap.set("n", "<leader>dl",
-        "<cmd>Telescope diagnostics<cr>", {buffer=0})
-
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-    vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, {buffer=0})
-end
-
-local lspconfig = require 'lspconfig'
-lspconfig.gopls.setup {
-    on_attach = lsp_on_attach
-}
+require("lsp")
 
 -- telescope
 local telescope = require 'telescope'
@@ -125,18 +124,17 @@ telescope.setup {
     },
 }
 
--- help
-vim.keymap.set("n", "<leader>h", '<cmd>Telescope help_tags<CR>')
+-- vim help
+kn("<leader>h", '<cmd>Telescope help_tags<CR>')
 
 -- git
--- gs - git status
-vim.keymap.set("n", "<leader>gs", ':Telescope git_status <CR>')
 -- gl - git log
-vim.keymap.set("n", "<leader>gla", ':Telescope git_commits <CR>')
+kn("<leader>gla", ':Telescope git_commits <CR>')
 -- glb - git log for current file
-vim.keymap.set("n", "<leader>glf", ':Telescope git_bcommits <CR>')
+kn("<leader>glf", ':Telescope git_bcommits <CR>')
 -- gb - git branch
-vim.keymap.set("n", "<leader>gb", ':Telescope git_branches <CR>')
+kn("<leader>gb", ':Telescope git_branches <CR>')
+
 -- colorscheme
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
@@ -154,3 +152,109 @@ vim.opt.hlsearch = false
 vim.opt.signcolumn = 'no'
 vim.opt.termguicolors = true
 
+
+
+
+
+
+-- tests
+function run_bash_command_and_send_output_to_new_buffer(cmd)
+    local handle = io.popen(cmd)
+    local result = handle:read('*a')
+    handle:close()
+
+    local lines = {}
+    for line in string.gmatch(result, "[^\n]+") do
+        table.insert(lines, line)
+    end
+
+    vim.cmd('vnew')
+    if #lines > 0 then
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    else
+        vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
+        vim.api.nvim_buf_set_option(0, 'bufhidden', 'delete')
+    end
+    vim.bo[0].filetype = 'sh'
+end
+
+local path_sep = "/"
+
+function split_string(input, separator)
+    local parts = {}
+    for part in string.gmatch(input, "([^" .. separator .. "]+)") do
+        table.insert(parts, part)
+    end
+    return parts
+end
+
+function split_path(path)
+    return split_string(path, path_sep)
+end
+
+function get_current_file_path()
+    return vim.fn.expand('%:p')
+end
+
+function getcwd()
+    return vim.fn.getcwd()
+end
+
+function get_current_file_path_relative_to_cwd()
+    local current_buffer = vim.api.nvim_get_current_buf()
+    local file_path = vim.api.nvim_buf_get_name(current_buffer)
+    local cwd = vim.fn.getcwd()
+    local relative_path = vim.fn.fnamemodify(file_path, ":~:.:.")
+    local path_relative_to_cwd = vim.fn.fnamemodify(relative_path, ":.")
+
+    return path_relative_to_cwd
+end
+
+function table_join(tbl, sep)
+    local result = ""
+    for i, v in ipairs(tbl) do
+        result = result .. v
+        if i < #tbl then
+            result = result .. sep
+        end
+    end
+    return result
+end
+
+function table_slice(tbl, first, last, step)
+    local sliced = {}
+    for i = first or 1, last or #tbl, step or 1 do
+        table.insert(sliced, tbl[i])
+    end
+    return sliced
+end
+
+function try_to_run_exam_tests()
+    local p = get_current_file_path_relative_to_cwd()
+    local sp = split_path(p)
+
+    if sp[1] ~= 'tests' then
+        print('not in tests directory')
+        return
+    end
+
+    local suite = sp[2]
+    local t = table_slice(sp, 3, #sp, 1)
+    t = table_join(t, '/')
+    local test = split_string(t, '.')[1]
+
+    run_tests_cmd = string.format(
+        "./backend test --modificators \"%s %s --no-colors\"", suite, test
+    )
+
+    print(suite)
+    print(test)
+    print(run_tests_cmd)
+    run_bash_command_and_send_output_to_new_buffer(run_tests_cmd)
+end
+
+function m()
+    try_to_run_exam_tests()
+end
+
+--kn('<leader>t', ':lua m()<CR>')
